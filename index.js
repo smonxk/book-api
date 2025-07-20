6c01e4e8e9ba5f734b660a229f7c6c364e3446c7
Simona Dobešová <160227355+smonxk@users.noreply.github.com>
2025-07-20 13:15:05 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -12,8 +12,24 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
 
+app.use((req, res, next) => {
+    res.locals.getFullYear = () => new Date().getFullYear();
+    res.locals.booksList = userBooks;
+    res.locals.urlCover = API_URL_cover;
+    next();
+});
+
+
 app.get("/", (req, res)=>{
-    res.render("index.ejs", {booksList : userBooks});
+    res.render("index.ejs");
+})
+
+app.get("/me", (req, res) =>{
+    res.render("me.ejs");
+})
+
+app.get("/user-shelf", (req,res)=>{
+    res.render("userShelf.ejs", {booksList: userBooks});
 })
 
 app.post("/find-book", async (req,res)=> {
@@ -22,13 +38,32 @@ app.post("/find-book", async (req,res)=> {
     try {
         const result = await axios.get(API_URL_info + "?q=title_suggest:" + req.body.searchContent);
         const books = result.data.docs;
-        console.log(books);
+        // console.log(books);
         
         // const editionKey =  firstEdition?.key?.split("/books/")[1];
         // console.log(editionKey);
         res.render("index.ejs", {booksList : userBooks, books: books, urlCover : API_URL_cover});
     } catch (error) {
         
+    }
+})
+
+app.post("/add-book", (req,res)=>{
+    try {
+        const bookJSON = req.body.addedBook;
+        const book = JSON.parse(bookJSON);
+        
+        const fullInfo = new Object();
+        fullInfo.book = book;
+        fullInfo.status = req.body.readingStatus;
+
+
+        console.log("the following is added in such form :" + JSON.stringify(fullInfo));
+        userBooks.push(fullInfo);
+        res.redirect("/");
+    } catch (error) {
+        console.error("Error parsing book:", error.message)
+        res.redirect("/");
     }
 })
 
