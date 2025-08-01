97721ab9cadda84c9db542f06df60ae0a61f8c99
Simona Dobešová <160227355+smonxk@users.noreply.github.com>
2025-08-01 11:30:36 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 import express from "express";
 import axios from "axios";
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from "path";
+import session from "express-session";
+
 
 const app = express();
 const port = 3000; 
 const API_URL_info = "https://openlibrary.org/search.json";
 const API_URL_cover = "https://covers.openlibrary.org/b/olid/";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 var userBooks = [];
+const simonaList = [];
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
+
+app.use(session({
+    secret: "b00k4w0rm5",
+    resave: false, 
+    saveUninitialized: true
+}))
 
 
 app.use((req, res, next) => {
     res.locals.getFullYear = () => new Date().getFullYear();
     res.locals.booksList = userBooks;
+    res.locals.userBookKeys = userBooks.map(b => b.book.key);   
     res.locals.urlCover = API_URL_cover;
+    res.locals.simonaList = simonaList;
     next();
 });
 
@@ -24,8 +40,16 @@ app.get("/", (req, res)=>{
     res.render("index.ejs");
 })
 
-app.get("/me", (req, res) =>{
+app.get("/me", async(req, res) =>{
+     
+    simonaList.length = 0; 
+    await fetchSimonaBook("Nightbane", "Alex Aster", "planning");
+    await fetchSimonaBook("2001", "Arthur C. Clarke", "reading");
+    await fetchSimonaBook("The gamification of learning and instruction", "Karl M. Kapp", "reading");
+
+
     res.render("me.ejs");
+    console.log(simonaList)
 })
 
 app.get("/user-shelf", (req,res)=>{
@@ -34,21 +58,18 @@ app.get("/user-shelf", (req,res)=>{
 
 app.post("/find-book", async (req,res)=> {
     console.log(JSON.stringify(req.body))
+    req.session.lastSearch = req.body.searchContent;;
 
     try {
         const result = await axios.get(API_URL_info + "?q=title_suggest:" + req.body.searchContent);
         const books = result.data.docs;
-        // console.log(books);
-        
-        // const editionKey =  firstEdition?.key?.split("/books/")[1];
-        // console.log(editionKey);
         res.render("index.ejs", {booksList : userBooks, books: books, urlCover : API_URL_cover});
     } catch (error) {
         
     }
 })
 
-app.post("/add-book", (req,res)=>{
+app.post("/add-book", async (req,res)=>{
     try {
         const bookJSON = req.body.addedBook;
         const book = JSON.parse(bookJSON);
@@ -57,10 +78,24 @@ app.post("/add-book", (req,res)=>{
         fullInfo.book = book;
         fullInfo.status = req.body.readingStatus;
 
-
-        console.log("the following is added in such form :" + JSON.stringify(fullInfo));
+        console.log("the following is being added in such form :" + JSON.stringify(fullInfo));
         userBooks.push(fullInfo);
-        res.redirect("/");
+
+        const query = req.session.lastSearch;
+        if(query){
+            const result = await axios.get(API_URL_info + "?q=title_suggest:"+ query );
+            const books = result.data.docs;
+
+            //update locals so that search result doesnt reset + added book label is still present
+            res.locals.booksList = userBooks;
+            res.locals.userBookKeys = userBooks.map(b => b.book.key);
+            res.locals.urlCover = API_URL_cover;
+
+
+            res.render("index.ejs", {books: books});
+        } else {
+            res.redirect("/")
+        }
     } catch (error) {
         console.error("Error parsing book:", error.message)
         res.redirect("/");
@@ -70,3 +105,30 @@ app.post("/add-book", (req,res)=>{
 app.listen(port, ()=>{
     console.log(`App is listening on port ${port}`);
 })
+
+async function fetchSimonaBook(title, author, status){
+    const res = await axios.get(API_URL_info, {
+        params: { title, author }
+    });
+
+    const book = res.data.docs[0];
+    if (!book) return;
+
+    const reasons = await fetchReasons();
+
+    const reasonObj = reasons.find(b => 
+        b.title.toLowerCase() === title.toLowerCase() &&
+        b.author.toLowerCase() === author.toLowerCase()
+    );
+
+    const reason = reasonObj ? reasonObj.reason : 'No reason provided';
+    const motivation = reasonObj ? reasonObj.motivation : 'No motivation provided';
+
+    simonaList.push({ book, status, reason, motivation });
+}
+
+async function fetchReasons() {
+    const filePath = path.join(__dirname, 'public', 'booksInfo.json');
+    const data = await fs.readFile(filePath, 'utf-8');
+    return JSON.parse(data);
+}
---
+++ public/index.js
@@ -0,0 +1,115 @@
+
+$(function() {
+    $(".reveal").on("click", function(){
+        const $form = $(this).closest("form");
+        const $status = $form.find(".status-wrapper");
+        
+        
+        $status.removeClass("hidden") .animate({
+            opacity: 1, height: "7em" 
+        }, 400);
+
+        const $item = $(this).closest(".book-item");
+        $item.find(".book-text").animate({ bottom: "20px" }, 600);
+        $item.find("img, .no-image").animate({ bottom: "20px" }, 600);
+
+        $(this).addClass("hidden");
+    });
+
+    $(".collide").on("click", function(e){
+        e.preventDefault();
+
+        const $form = $(this).closest("form");
+        const $status = $form.find(".status-wrapper");
+
+        $status.animate({
+            opacity: 0, height: "0" 
+        }, 400, function(){
+            $status.addClass("hidden");
+        });
+        
+        const $item = $(this).closest(".book-item");
+        $item.find(".book-text").animate({ bottom: "0px" }, 600);
+        $item.find("img, .no-image").animate({ bottom: "0px" }, 600);
+
+        $form.find(".reveal").css({ opacity: 0, position: "relative", top: "-20px"});
+        $form.find(".reveal").removeClass("hidden").animate({ opacity: 1, top: "0px"}, 400);
+    });
+
+    $(".book-text h4").each(function(){
+        const lineHeight = parseFloat($(this).css("line-height"));
+        const elementHeight = $(this).outerHeight();
+        const lines = Math.round(elementHeight / lineHeight);
+        let fontSize = parseFloat($(this).css("font-size"));
+
+        if(lines > 1){
+            fontSize -= 15;
+            $(this).css("font-size", fontSize + "px");
+        } 
+    });
+
+    function isInViewport(e){
+        const rect = e[0].getBoundingClientRect();
+        return (
+            rect.bottom > 0 &&
+            rect.top <= window.innerHeight - 50 //50px odshora elementu je ve viewportu
+        )
+    }
+    const $items = $("#search-result .book-item");
+
+    $items.css({
+        opacity: 0, position: "relative", top: "20px"
+    })
+
+    $("#socials").css({ opacity: 0, top: "-30px" })
+
+    function revealVisibleItems(){
+        let delayCounter = 0;
+        $items.each(function (){
+            const $item = $(this);
+
+            if($item.data("revealed")) return;
+
+            if(isInViewport($item)){
+                $item.data("revealed", true);
+
+                setTimeout(() =>{
+                    $item.animate({
+                        opacity: 1, top: "0px"
+                    }, 600);
+                }, delayCounter *150);
+                delayCounter++;
+            }
+        })
+    }
+
+    $(window).on("scroll resize", revealVisibleItems);
+    setTimeout(revealVisibleItems, 100);
+
+    $(".status-wrapper").css({
+        opacity: 0, height: 0, overflow: "hidden"
+    });
+
+$("#simona-full-name").on("click", function(){
+    const $links = $("#socials");
+    const $books = $("#simona-books");
+
+    if($links.hasClass("closed")){
+        $links.css({ opacity: 0, top: "-30px"}).removeClass("closed");
+        $links.animate({opacity: 1, top: "-10px"}, 400);
+        
+        $books.css({transform: "translateY(10px)"});
+        
+        $links.find("a, p").css({zIndex: 5});
+    } else {
+        $links.find("a, p").css({zIndex: -1});
+        
+        $links.animate({opacity: 0, top: "-30px"}, 400, function(){
+            $links.addClass("closed");
+        });
+        
+        $books.css({transform: "translateY(0px)"});
+    }
+});
+
+});
